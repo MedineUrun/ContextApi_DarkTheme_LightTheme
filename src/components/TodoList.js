169fc0bffdca24b8a980e5cd8bb4841c38afbfdd
MedineUrun <52495729+MedineUrun@users.noreply.github.com>
2022-04-20 14:32:22 +0300

    Medine

--- src/components/TodoList.js
+++ src/components/TodoList.js
@@ -1,7 +1,6 @@
 import React from "react";
 import { ThemeContext } from '../contexts/ThemeContext';
 
-
 class TodoList extends React.Component {
     static contextType = ThemeContext;
     render() {
