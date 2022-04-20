9e17d89282aff346cc2666d8db0cf177f185a222
MedineUrun <52495729+MedineUrun@users.noreply.github.com>
2022-04-20 10:23:57 +0300

    Medine

---
+++ src/components/TodoList.js
@@ -0,0 +1,20 @@
+import React from "react";
+import { ThemeContext } from '../contexts/ThemeContext';
+
+
+class TodoList extends React.Component {
+    static contextType = ThemeContext;
+    render() {
+        const { isDarkTheme, darkTheme, lightTheme } = this.context;
+        const theme = isDarkTheme ? darkTheme : lightTheme;
+        return(
+            <div style={{ background: theme.background, color: theme.text, height:'120px'}} className="ui List">
+                <p className="item">Plan the family trip</p>
+                <p className="item">Go for shopping for dinner</p>
+                <p className="item">Go for a walk</p>
+            </div>
+        )
+    }
+}
+
+export default TodoList;
