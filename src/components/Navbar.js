eda6d6dfca6b046769d7353a495030fabd2737f7
MedineUrun <52495729+MedineUrun@users.noreply.github.com>
2022-04-20 10:43:27 +0300

    Medine

--- src/components/Navbar.js
+++ src/components/Navbar.js
@@ -3,20 +3,25 @@ import { ThemeContext } from "../contexts/ThemeContext";
 
 
 class Navbar extends React.Component {
-    static contextType = ThemeContext;
     render(){
-        const { isDarkTheme, darkTheme, lightTheme } = this.context;
-        const theme = isDarkTheme ? darkTheme : lightTheme;
         console.log(this.context);
-        return (
-            <nav style={{ background: theme.background, color: theme.text, height:'120px'}}>
-                <h2 style={{ textAlign:'center'}} className="ui centered header">Photo Blog</h2>
-                <div className="ui three buttons">
-                    <button className="ui button">Overview</button>
-                    <button className="ui button">Contact</button>
-                    <button className="ui button">Support</button>
-                </div>
-            </nav>
+        return(
+        <ThemeContext.Consumer>{(context) => {
+            const { isDarkTheme, darkTheme, lightTheme } = context;
+            const theme = isDarkTheme ? darkTheme : lightTheme;
+            return(
+                <nav style={{ background: theme.background, color: theme.text, height:'120px'}}>
+                    <h2 style={{ textAlign:'center'}} className="ui centered header">To Do List</h2>
+                    <div className="ui three buttons">
+                        <button className="ui button">Overview</button>
+                        <button className="ui button">Contact</button>
+                        <button className="ui button">Support</button>
+                    </div>
+                </nav> 
+            )
+            }}
+
+        </ThemeContext.Consumer>
         )
     }
 }
