e76887b175aa3b32bc862b32cb0b949d46130588
MedineUrun <52495729+MedineUrun@users.noreply.github.com>
2022-04-20 14:32:27 +0300

    medine

--- src/App.js
+++ src/App.js
@@ -1,16 +1,20 @@
 import React from "react";
 import Navbar from "./components/Navbar";
 import TodoList from "./components/TodoList";
+import AuthContextProvider from "./contexts/AuthContext";
 import ThemeContextProvider from "./contexts/ThemeContext";
+
 
 
 function App() {
   return (
     <div className="App">
       <div className="ui raised very padded text container segment">
-        <ThemeContextProvider>
+      <ThemeContextProvider>
+        <AuthContextProvider>
         <Navbar />
         <TodoList />
+        </AuthContextProvider>   
         </ThemeContextProvider>
       </div>
     </div>
--- src/components/Navbar.js
+++ src/components/Navbar.js
@@ -1,27 +1,30 @@
 import React from "react";
 import { ThemeContext } from "../contexts/ThemeContext";
-
+import { AuthContext } from "../contexts/AuthContext";
 
 class Navbar extends React.Component {
-    render(){
-        console.log(this.context);
-        return(
-        <ThemeContext.Consumer>{(context) => {
-            const { isDarkTheme, darkTheme, lightTheme } = context;
-            const theme = isDarkTheme ? darkTheme : lightTheme;
-            return(
-                <nav style={{ background: theme.background, color: theme.text, height:'120px'}}>
-                    <h2 style={{ textAlign:'center'}} className="ui centered header">To Do List</h2>
-                    <div className="ui three buttons">
-                        <button className="ui button">Overview</button>
-                        <button className="ui button">Contact</button>
-                        <button className="ui button">Support</button>
-                    </div>
-                </nav> 
-            )
-            }}
-
-        </ThemeContext.Consumer>
+    render() {
+        return (
+            <AuthContext.Consumer>{(authContext) => (
+                <ThemeContext.Consumer>{(themeContext) => {
+                    const { isDarkTheme, darkTheme, lightTheme } = themeContext;
+                    const { isLoggedIn, changeAuthStatus } = authContext;
+                    const theme = isDarkTheme ? darkTheme : lightTheme;
+                    return (
+                        <nav style={{ background: theme.background, color: theme.text, height: '120px' }}>
+                            <h2 style={{ textAlign: 'center' }} className="ui centered header">To Do List</h2>
+                            <p style={{textAlign: 'center'}} onClick={ changeAuthStatus }>{ isLoggedIn ? 'Logged in' : 'Loggen out' }</p>
+                            <div className="ui three buttons">
+                                <button className="ui button">Overview</button>
+                                <button className="ui button">Contact</button>
+                                <button className="ui button">Support</button>
+                            </div>
+                        </nav>
+                    )
+                }}
+                </ThemeContext.Consumer>
+            )}
+            </AuthContext.Consumer>
         )
     }
 }
